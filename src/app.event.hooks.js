245c601720148e232e9241f993bba71eae44547e
Santiago <sgobotta@gmail.com>
2018-06-17 18:21:52 -0300

    Queues to the event Hooks a function to load fixtures

--- src/app.event.hooks.js
+++ src/app.event.hooks.js
@@ -1,4 +1,9 @@
+const Hooks = require('./hooks/event-hooks');
+const loadFixturesJob = require('./lib/fixtures');
 
 module.exports = function (app) {
 
+  if (!process.env.SKIP_FIXTURES) {
+    Hooks.add('beforeCoreInit', loadFixturesJob(app));
+  }
 };
