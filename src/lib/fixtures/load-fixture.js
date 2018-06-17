dd02dfb49709959d293a6dd7ec899057a4a0a416
Santiago <sgobotta@gmail.com>
2018-06-17 18:19:32 -0300

    Adds a function to add assets into mongoose collections

---
+++ src/lib/fixtures/load-fixture.js
@@ -0,0 +1,31 @@
+const loadFixture = function(ModelClass, data) {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(data)) {
+      // Load array of json objects
+      const promises = [];
+
+      data.forEach((json) => {
+        promises.push((new ModelClass(json)).save());
+      });
+
+      Promise.all(promises)
+        .then((results) => {
+          resolve(results);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    } else {
+      // Load single json object
+      (new ModelClass(data)).save()
+        .then((inst) => {
+          resolve(inst);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }
+  });
+};
+
+module.exports = loadFixture;
