5bb4768f98d0ba3d7d90803fd2eadff8ab73db3a
Santiago <sgobotta@gmail.com>
2018-06-17 18:22:35 -0300

    Runs an event hook before server initialisation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
+const Hooks = require('./hooks/event-hooks');
+
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
+
+Hooks.run('beforeCoreInit');
 
 server.on('listening', () =>
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
