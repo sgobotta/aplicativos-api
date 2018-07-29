4db05361d305a84c3e202f138b7b634fb4f9fae1
Santiago <sgobotta@gmail.com>
2018-07-29 13:30:29 -0300

    Removes unused properties in users model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,19 +5,8 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
     email: {type: String, unique: true},
     password: { type: String },
-  
-  
-    auth0Id: { type: String },
-  
-    googleId: { type: String },
-  
-    facebookId: { type: String },
-  
-    githubId: { type: String },
-  
   }, {
     timestamps: true
   });
