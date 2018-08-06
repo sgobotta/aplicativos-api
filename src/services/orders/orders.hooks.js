61846a180456957d45f2088ea15237fc08b78934
Santiago <sgobotta@gmail.com>
2018-08-06 01:02:04 -0300

    Implements a before hook with populate and after hook with order filtering

--- src/services/orders/orders.hooks.js
+++ src/services/orders/orders.hooks.js
@@ -1,23 +1,59 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+/**
+ * @function skipFeathersInternalCall
+ * @summary Given a context checks wheter the call is a request or a feathers
+ * intenal call.
+ * @param {Object} context A before hook context.
+ * @returns {Object} A before hook context.
+ */
+const skipFeathersInternalCall = (context) => {
+  if(!context.params.provider) {
+    return context;
+  }
+};
+
+function projectAuthors(context) {
+  context.params.query = {
+    $populate: 'author'
+  };
+  return context;
+}
+
+function filterData(context) {
+  skipFeathersInternalCall(context);
+  const orders = context.result.map((order) => {
+    return {
+      _id: order._id,
+      isActive: order.isActive,
+      author: {
+        _id: order.author._id,
+        username: order.author.username
+      }
+    };
+  });
+  context.params.query = {};
+  context.result = orders;
+  return context;
+}
 
 module.exports = {
   before: {
     all: [],
     find: [
-      hook => { console.log('find', hook.params); return hook; }
+      projectAuthors
     ],
-    get: [ authenticate('jwt') ],
-    create: [
-      hook => { console.log('create', hook.params); return hook; }
-    ],
-    update: [ authenticate('jwt') ],
-    patch: [ authenticate('jwt') ],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
     remove: []
   },
 
   after: {
-    all: [],
-    find: [],
+    all: [
+    ],
+    find: [
+      filterData
+    ],
     get: [],
     create: [],
     update: [],
