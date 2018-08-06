5bdaa459348bffd3996f319a1d6674272b34eb7e
Santiago <sgobotta@gmail.com>
2018-08-05 23:50:17 -0300

    Adds email and username to user model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,8 +5,9 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-    email: {type: String, unique: true},
+    email: { type: String, unique: true, required: true },
     password: { type: String },
+    username: { type: String, unique: true, required: true },
   }, {
     timestamps: true
   });
