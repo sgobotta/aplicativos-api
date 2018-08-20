757385b2c56315980e333ca51bff8aca1c5a9cdb
Santiago <sgobotta@gmail.com>
2018-08-20 05:31:30 -0300

    Orders Model: Fixes the ref attribute

--- src/models/orders.model.js
+++ src/models/orders.model.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
   const { Schema } = mongooseClient;
 
   const OrderParticipant = new mongooseClient.Schema({
-    participantId: { type: Schema.Types.ObjectId, ref: 'participantId', required: true},
+    participantId: { type: Schema.Types.ObjectId, ref: 'users', required: true},
     selection: { type: [String]}
   });
 
