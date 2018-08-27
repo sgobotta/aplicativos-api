476f219d95696cbd88a2331eabee7bf90c5be274
Santiago <sgobotta@gmail.com>
2018-08-26 22:26:01 -0300

    Users Service: patch hook update
    + Adds a hook to determine which fields have been modified for the update request

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -4,6 +4,17 @@ const {
   hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
 
+function filterUnassignedAttributes(context) {
+  const { data } = context;
+  const keys = Object.keys(data);
+  keys.forEach((key) => {
+    if (!data[key]) {
+      delete data[key];
+    }
+  });
+  return context;
+}
+
 module.exports = {
   before: {
     all: [],
@@ -11,7 +22,11 @@ module.exports = {
     get: [ authenticate('jwt') ],
     create: [ hashPassword() ],
     update: [ hashPassword(),  authenticate('jwt') ],
-    patch: [ hashPassword(),  authenticate('jwt') ],
+    patch: [
+      hashPassword(),
+      authenticate('jwt'),
+      filterUnassignedAttributes
+    ],
     remove: [ authenticate('jwt') ]
   },
 
